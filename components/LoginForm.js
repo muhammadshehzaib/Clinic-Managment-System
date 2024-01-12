bbdd2e90d69455f9440b04f28d9648747e2d94ef
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-01-12 15:44:21 +0500

    vercel deployments issues resolving

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -56,9 +56,9 @@ export default function LoginForm() {
               {error}
             </div>
           )}
-
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>
+            Don&apos;t have an account?{" "}
+            <span className="underline">Register</span>
           </Link>
         </form>
       </div>
